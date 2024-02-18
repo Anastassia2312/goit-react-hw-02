08f496702a3eaa47698db33265fd02cbe353a24c
Anastassia2312 <23122000na@gmail.com>
2024-02-18 18:49:45 +0200

    Update Options.jsx

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -4,7 +4,7 @@ export default function Feedback({ totalFeedback, updateFeedback, onReset }) {
       <button onClick={() => updateFeedback("good")}>Good</button>
       <button onClick={() => updateFeedback("neutral")}>Neutral</button>
       <button onClick={() => updateFeedback("bad")}>Bad</button>
-      {totalFeedback !== 0 && <button onClick={onReset}>Bad</button>}
+      {totalFeedback !== 0 && <button onClick={onReset}>Reset</button>}
     </div>
   );
 }
