1d01b0c3e9a8a9da686baa3883eff58b5d6d6aa6
Anastassia2312 <23122000na@gmail.com>
2024-02-18 19:24:31 +0200

    css

--- src/components/Options/Options.jsx
+++ src/components/Options/Options.jsx
@@ -1,10 +1,21 @@
+import css from "./Options.module.css";
 export default function Feedback({ totalFeedback, updateFeedback, onReset }) {
   return (
-    <div>
-      <button onClick={() => updateFeedback("good")}>Good</button>
-      <button onClick={() => updateFeedback("neutral")}>Neutral</button>
-      <button onClick={() => updateFeedback("bad")}>Bad</button>
-      {totalFeedback !== 0 && <button onClick={onReset}>Reset</button>}
+    <div className={css.options}>
+      <button className={css.button} onClick={() => updateFeedback("good")}>
+        Good
+      </button>
+      <button className={css.button} onClick={() => updateFeedback("neutral")}>
+        Neutral
+      </button>
+      <button className={css.button} onClick={() => updateFeedback("bad")}>
+        Bad
+      </button>
+      {totalFeedback !== 0 && (
+        <button className={css.button} onClick={onReset}>
+          Reset
+        </button>
+      )}
     </div>
   );
 }
