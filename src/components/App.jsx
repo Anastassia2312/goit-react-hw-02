45a75c380d2dd3f7b160d6a9d760303c5ed19fb9
Anastassia2312 <23122000na@gmail.com>
2024-02-18 19:05:50 +0200

    localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Description from "./Description/Description";
 import Feedback from "./Feedback/Feedback";
@@ -6,10 +6,17 @@ import Options from "./Options/Options";
 import Notification from "./Notification/Notification";
 
 function App() {
-  const [rating, setRating] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [rating, setRating] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+
+    if (savedFeedback !== null) {
+      return savedFeedback;
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
   });
   const updateFeedback = (feedbackType) => {
     setRating((feedback) => ({
@@ -29,6 +36,9 @@ function App() {
   const positiveFeedback = Math.round(
     ((rating.good + rating.neutral) / totalFeedback) * 100
   );
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(rating));
+  });
 
   return (
     <>
